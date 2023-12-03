75362f3c5ad5ff5ec756042ccea4e7c90aad56a1
ivan <ivanmoll07@gmail.com>
2023-12-03 18:58:07 +0100

    Refactor manufacture function to use Set for materials

--- challenge-02/solution.js
+++ challenge-02/solution.js
@@ -1,24 +1,8 @@
 function manufacture(gifts, materials) {
-  const canBeMade = [];
-  let canBeAdded = true;
-  const materialsSplitted = materials.split("");
-
-  for (const gift of gifts) {
-    const giftSplitted = gift.split("");
-
-    for (const letter of giftSplitted) {
-      if (!materialsSplitted.includes(letter)) {
-        canBeAdded = false;
-      }
-    }
-
-    if (canBeAdded) {
-      canBeMade.push(gift);
-      canBeAdded = true;
-    }
-  }
-
-  return canBeMade;
+  const materialsSet = new Set(materials);
+  return gifts.filter(
+    (gift) => !Array.from(gift).some((letter) => !materialsSet.has(letter))
+  );
 }
 
 export default manufacture;
