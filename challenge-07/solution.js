91a0efd346730683bac616e7d1d0917b22fc8569
ivan <ivanmoll07@gmail.com>
2023-12-07 18:49:53 +0100

    Working on solution for Challenge 7: The 3D boxes

--- challenge-07/solution.js
+++ challenge-07/solution.js
@@ -2,29 +2,39 @@ function drawGift(size, symbol) {
   const giftHeightAndWidth = (size - 1) * 2 + 1; // 7
   const gift = [];
   const PAD = "#";
-  let start = size - 1// 4
-  const end = giftHeightAndWidth
+  const SPACE = " ";
+  const SYMBOL = symbol;
+  let start = size - 1; // 4
+  const end = giftHeightAndWidth;
   let giftLine;
 
   for (let i = 0; i < size; i++) {
-    giftLine = Array(giftHeightAndWidth).fill("").map((_, i) => {
-      if (i >= start && i <= end) {
-        return PAD
-      } else if () {
-        return symbol
-      } else {
-        return " "
-      }
-    }).join("");
+    giftLine = Array(giftHeightAndWidth)
+      .fill("")
+      .map((_, j) => {
+        if (i === 0) {
+          return j >= start && j < end ? PAD : SPACE;
+        } else {
+          if (j === start + 1 || j === start + 2) {
+            return SYMBOL;
+          } else if (j === start || (j > start && j < end)) {
+            return PAD;
+          } else {
+            return SPACE;
+          }
+        }
+      })
+      .join("");
 
     gift.push(giftLine);
 
-    start--
+    start--;
   }
 
   return gift.join("\n");
 }
 
-console.log(drawGift(4, '+'))
+// console.log(drawGift(4, "+"));
+console.log(drawGift(5, "*"));
 
 export default drawGift;
