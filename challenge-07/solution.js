ec1fe1973e7369df4427b1dc309242c479771870
ivan <ivanmoll07@gmail.com>
2023-12-11 23:36:20 +0100

    Refactor drawGift function

--- challenge-07/solution.js
+++ challenge-07/solution.js
@@ -1,46 +1,49 @@
 function drawGift(size, symbol) {
   const totalHeight = (size - 1) * 2 + 1;
   const backFillHeight = totalHeight - 2;
-  const backFill = (size + 1) / 2;
+  const backFill = size - 2;
   const middle = Math.ceil(backFillHeight / 2);
-  let lineCount = middle - 1;
+  let dynamicBackFill = 0;
+  let upperSpace = backFill;
 
   const PAD = "#";
   const SPACE = " ";
   const SYMBOL = symbol;
+  const NEWLINE = "\n";
 
-  let gift = "";
+  if (size < 2) return "#" + NEWLINE;
 
-  gift = SPACE.repeat(middle) + PAD.repeat(size) + "\n";
+  let gift = SPACE.repeat(middle) + PAD.repeat(size) + NEWLINE;
 
   for (let h = 1; h <= backFillHeight; h++) {
     if (h < middle) {
       gift +=
-        SPACE.repeat(lineCount) +
+        SPACE.repeat(upperSpace) +
         PAD.repeat(1) +
         SYMBOL.repeat(backFill) +
         PAD.repeat(1) +
-        SYMBOL.repeat(backFill - lineCount) +
+        SYMBOL.repeat(dynamicBackFill) +
         PAD.repeat(1) +
-        "\n";
-      lineCount--;
+        NEWLINE;
+      dynamicBackFill++;
+      upperSpace--;
     } else if (h === middle) {
-      gift += PAD.repeat(size) + SYMBOL.repeat(backFill) + PAD.repeat(1) + "\n";
-      lineCount = 1;
+      gift +=
+        PAD.repeat(size) + SYMBOL.repeat(backFill) + PAD.repeat(1) + NEWLINE;
+      dynamicBackFill--;
     } else if (h > middle) {
       gift +=
         PAD.repeat(1) +
         SYMBOL.repeat(backFill) +
         PAD.repeat(1) +
-        SYMBOL.repeat(backFill - lineCount) +
+        SYMBOL.repeat(dynamicBackFill) +
         PAD.repeat(1) +
-        SPACE.repeat(lineCount) +
-        "\n";
-      lineCount++;
+        NEWLINE;
+      dynamicBackFill--;
     }
   }
 
-  gift += PAD.repeat(size) + SPACE.repeat(middle) + "\n";
+  gift += PAD.repeat(size) + NEWLINE;
 
   return gift;
 }
--- challenge-07/solution.test.js
+++ challenge-07/solution.test.js
@@ -8,22 +8,21 @@ describe("Challange-07 tests", () => {
     assert.strictEqual(typeof result, "string");
   });
 
-  test("", () => {
-    const expected = `
-   ####
+  test("Test: drawGift(4, '+')", () => {
+    const expected = `   ####
   #++##
  #++#+#
 ####++#
 #++#+#
 #++##
-####`;
+####
+`;
     const actual = drawGift(4, "+");
     assert.strictEqual(actual, expected);
   });
 
-  test("", () => {
-    const expected = `
-    #####
+  test("Test: drawGift(5, '*')", () => {
+    const expected = `    #####
    #***##
   #***#*#
  #***#**#
@@ -31,26 +30,50 @@ describe("Challange-07 tests", () => {
 #***#**#
 #***#*#
 #***##
-#####`;
+#####
+`;
     const actual = drawGift(5, "*");
     assert.strictEqual(actual, expected);
   });
 
-  // test("", () => {
-  //   const expected = 5;
-  //   const actual = drawGift(">***>");
-  //   assert.strictEqual(actual, expected);
-  // });
+  test("Test: drawGift(1, '^')", () => {
+    const expected = `#
+`;
+    const actual = drawGift(1, "^");
+    assert.strictEqual(actual, expected);
+  });
 
-  // test("", () => {
-  //   const expected = 10;
-  //   const actual = drawGift("**********");
-  //   assert.strictEqual(actual, expected);
-  // });
+  test("Test: drawGift(2, '&')", () => {
+    const expected = ` ##
+###
+##
+`;
+    const actual = drawGift(2, "&");
+    assert.strictEqual(actual, expected);
+  });
 
-  // test("", () => {
-  //   const expected = 2;
-  //   const actual = drawGift("<<**>>");
-  //   assert.strictEqual(actual, expected);
-  // });
+  test("", () => {
+    const expected = `         ##########
+        #%%%%%%%%##
+       #%%%%%%%%#%#
+      #%%%%%%%%#%%#
+     #%%%%%%%%#%%%#
+    #%%%%%%%%#%%%%#
+   #%%%%%%%%#%%%%%#
+  #%%%%%%%%#%%%%%%#
+ #%%%%%%%%#%%%%%%%#
+##########%%%%%%%%#
+#%%%%%%%%#%%%%%%%#
+#%%%%%%%%#%%%%%%#
+#%%%%%%%%#%%%%%#
+#%%%%%%%%#%%%%#
+#%%%%%%%%#%%%#
+#%%%%%%%%#%%#
+#%%%%%%%%#%#
+#%%%%%%%%##
+##########
+`;
+    const actual = drawGift(10, "%");
+    assert.strictEqual(actual, expected);
+  });
 });
